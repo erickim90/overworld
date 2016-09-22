89659d2610688e940f11a5b996790e23b1f981a6
eckim90 <eric@bluepandalabs.com>
2016-09-22 15:13:07 -0700

    Create Contact Page, Added more github profile information, added font-awesome icons, loading icon, css changes

--- public/scripts/info.js
+++ public/scripts/info.js
@@ -26,25 +26,69 @@
     }
 
     jqueryReady(function ($) {
+        //Hide content and show spinner on page load
+        var loadingSpinner = $('#loadingDiv');
+        var $loadingGithub = $('#loadingGithub');
+
+        //Show content and hide spinner on ajax complete
+        $(document)
+            .ajaxStart(function () {
+                loadingSpinner.show();
+            })
+            .ajaxStop(function () {
+                loadingSpinner.hide();
+                $loadingGithub.show();
+            });
+
+        // custom library for github calendar
+        // https://github.com/IonicaBizau/github-calendar
+        // Embed your GitHub calendar everywhere.
         GitHubCalendar(".calendar", "erickim90");
+
+        //Ajax request to github API
         $.get( "https://api.github.com/users/erickim90", function(data) {
-            console.log(data)
-            $("#eck-github-profile").append(
+            console.log(data);
+            $("#eck-github-profile").prepend(
+                //Append returned data
                 '<div class="mdl-card__media"><img src="'+data.avatar_url+'" class="width-percent-100"></div>'
-                +'<div class="mdl-grid">'
+                +'<div class="mdl-grid padding-bottom-0">'
                 +'<h3  class="mdl-cell mdl-cell--12-col mdl-typography--headline margin-bottom-0 eck-github-name">'+data.name+'</h3>'
                 +'<h6  class="mdl-cell mdl-cell--12-col margin-top-0 eck-github-username">'+data.login+'</h6>'
                 +'<div class="mdl-cell mdl-cell--12-col mdl-card__supporting-text padding-0 eck-github-bio">'+data.bio+'</div>'
-                +'</div>');
+                +'</div>'
+                +'<div> <ul class="mdl-list">'
+                +'<li><hr class="margin-top-0"></li>'
+                +'<li class="mdl-list__item eck-github-list">'
+                +'<span class="mdl-list__item-primary-content">'
+                +'<i class="fa fa-phone eck-github-icons" aria-hidden="true"></i>Phone'
+                +'</span> </li>'
+                +'<li class="mdl-list__item eck-github-list">'
+                +'<span class="mdl-list__item-primary-content">'
+                +'<i class="fa fa-users eck-github-icons" aria-hidden="true"></i>'+data.company
+                +'</span> </li>'
+                +'<li class="mdl-list__item eck-github-list">'
+                +'<span class="mdl-list__item-primary-content">'
+                +'<i class="fa fa-location-arrow eck-github-icons" aria-hidden="true"></i>'+data.location
+                +'</span> </li>'
+                +'<li class="mdl-list__item eck-github-list">'
+                +'<span class="mdl-list__item-primary-content">'
+                +'<i class="fa fa-envelope-o eck-github-icons" aria-hidden="true"></i>'+data.email
+                +'</span> </li>'
+                +'<li class="mdl-list__item eck-github-list">'
+                +'<span class="mdl-list__item-primary-content">'
+                +'<i class="fa fa-clock-o eck-github-icons" aria-hidden="true"></i>' + 'Joined '+moment(data.created_at).format('MMM DD YYYY')
+                +'</span> </li> </ul> </div>'
+            )
+
+
         })
-            .done(function() {
-                console.log( "Finished AJAX call" );
-            })
-            .fail(function() {
-                alert( "There was an error" );
-            })
-            .always(function() {
-                console.log( "Done" );
-            });
+        // .done(function() {})
+            .fail(function() {alert( "There was an error" );});
+        // .always(function() {});
+
+
+
     });
 })();
+
+
